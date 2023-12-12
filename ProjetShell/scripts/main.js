922b0ec909352f63e26eb3cc74ebe166165ed7fb
abd3louhab3 <112757317+abd3louhab3@users.noreply.github.com>
2023-12-12 16:05:51 +0100

    Update main.js

--- ProjetShell/scripts/main.js
+++ ProjetShell/scripts/main.js
@@ -1,3 +1,12 @@
 function handleClick(){
     document.innerhtml("<p>  branch principal </p>")
 }; 
+function finalValidate()
+        {
+          var count = 0;
+          for(i=0;i<6;i++)
+          {
+            var div = divs[i];
+            if(document.getElementById(div).innerHTML == "OK!")
+            count = count + 1;
+          }
