1b7634462f224f15e3be96c514f16b0b6f12bc17
abd3louhab3 <112757317+abd3louhab3@users.noreply.github.com>
2023-12-12 16:04:50 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function finalValidate()
+        {
+          var count = 0;
+          for(i=0;i<6;i++)
+          {
+            var div = divs[i];
+            if(document.getElementById(div).innerHTML == "OK!")
+            count = count + 1;
+          }
