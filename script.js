2d6d0e8d67b5bd2648fe6265fff6aa0b82d26d28
abd3louhab3 <112757317+abd3louhab3@users.noreply.github.com>
2023-12-12 17:31:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,4 @@ function finalValidate()
             count = count + 1;
           }
 function finalValidate2()
-        {
-          var count = 0;
-          for(i=0;i<6;i++)
-          {
-            var div = divs[i];
-            if(document.getElementById(div).innerHTML == "OK!")
-            count = count + 1;
-          }
+ 
